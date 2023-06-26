68850c0a3df8d6826fd3c556df06ec4b5c57d8da
Alex Tușinean <alex@tusinean.ro>
2023-06-26 22:25:07 +0000

    fix metadata

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,6 @@ export default function Home() {
     setCurrentYear(new Date().getFullYear());
   }, []);
 
-  if (!currentYear)
-    return;
-
   return (
     <>
       <Head>
@@ -33,36 +30,41 @@ export default function Home() {
           content='EasySave3 Editor helps you empower your gaming journey with effortless save file editing. Seamlessly modify, and manage EasySave3 game saves with a user-friendly web application designed to enhance your gaming experience.'
         />
       </Head>
-      <Flex alignItems='center' justifyContent='center' mt='24' mb='14'>
-        <a ref={downloader} id={'downloader'} style={{ display: 'none' }}></a>
-        <Box
-          direction='column'
-          background='gray.700'
-          rounded='6'
-          p='12'
-          position='relative'
-        >
-          <Heading mb='6'>EasySave3 Editor</Heading>
 
-          <Text>Password</Text>
-          <Input value={password} placeholder='a1bc2d3fghi4...' onChange={e => setPassword(e.target.value)} isDisabled={isOpening} isRequired />
-          <Text mb={6}>Use <Code>t36gref9u84y7f43g</Code> for Phasmophobia</Text>
+      {currentYear && (
+        <>
+          <Flex alignItems='center' justifyContent='center' mt='24' mb='14'>
+            <a ref={downloader} id={'downloader'} style={{ display: 'none' }}></a>
+            <Box
+              direction='column'
+              background='gray.700'
+              rounded='6'
+              p='12'
+              position='relative'
+            >
+              <Heading mb='6'>EasySave3 Editor</Heading>
 
-          <Divider mt='5' mb='3' />
-          <Heading size='md' mb='3'>Decryption</Heading>
-          <DecryptionForm downloader={downloader} isOpening={isOpening} setIsOpening={setIsOpening} password={password} />
+              <Text>Password</Text>
+              <Input value={password} placeholder='a1bc2d3fghi4...' onChange={e => setPassword(e.target.value)} isDisabled={isOpening} isRequired />
+              <Text mb={6}>Use <Code>t36gref9u84y7f43g</Code> for Phasmophobia</Text>
 
-          <Divider mt='5' mb='3' />
-          <Heading size='md' mb='3'>Encryption</Heading>
-          <EncryptionForm downloader={downloader} isOpening={isOpening} setIsOpening={setIsOpening} password={password} />
-        </Box>
-      </Flex>
+              <Divider mt='5' mb='3' />
+              <Heading size='md' mb='3'>Decryption</Heading>
+              <DecryptionForm downloader={downloader} isOpening={isOpening} setIsOpening={setIsOpening} password={password} />
 
-      <Box position='fixed' bottom='5px' right='5px' textAlign='right'>
-        <Link as={NextLink} href='/terms-of-service' color='blue.500' mr='6.5px'>Terms of Service</Link>
-        <Link as={NextLink} href='/privacy-policy' color='blue.500'>Privacy Policy</Link>
-        <Text>Copyright © {currentYear} <Link as={NextLink} href='https://alex.tusinean.ro' color='blue.500'>Alex Tușinean</Link></Text>
-      </Box>
+              <Divider mt='5' mb='3' />
+              <Heading size='md' mb='3'>Encryption</Heading>
+              <EncryptionForm downloader={downloader} isOpening={isOpening} setIsOpening={setIsOpening} password={password} />
+            </Box>
+          </Flex>
+
+          <Box position='fixed' bottom='5px' right='5px' textAlign='right'>
+            <Link as={NextLink} href='/terms-of-service' color='blue.500' mr='6.5px'>Terms of Service</Link>
+            <Link as={NextLink} href='/privacy-policy' color='blue.500'>Privacy Policy</Link>
+            <Text>Copyright © {currentYear} <Link as={NextLink} href='https://alex.tusinean.ro' color='blue.500'>Alex Tușinean</Link></Text>
+          </Box>
+        </>
+      )}
     </>
   );
 }
