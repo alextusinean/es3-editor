d4ff6cbaae31a93cf42b0d2c4f7c44a813f62510
Alex Tușinean <alex@tusinean.ro>
2023-06-26 17:17:20 +0000

    move ga scripts in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,24 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang='en'>
-        <Head />
+        <Head>
+          {/* please remove/change these two scripts if you're launching your own instance */}
+          <Script strategy='beforeInteractive' src='https://www.googletagmanager.com/gtag/js?id=G-X7X0P5WB4F' />
+          <Script id='google-analytics-initialize' strategy='beforeInteractive'>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-X7X0P5WB4F');
+            `}
+          </Script>
+        </Head>
         <body>
           <ColorModeScript initialColorMode='dark' />
           <Main />
