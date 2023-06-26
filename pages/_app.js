92fbc3417aee4c49bf8f693b634c0e3411ab57b8
Alex Tușinean <alex@tusinean.ro>
2023-06-26 19:25:22 +0000

    move Head in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>EasySave3 Editor</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import { Box, Code, Divider, Flex, Heading, Input, Text } from '@chakra-ui/react
 import { useRef, useState } from 'react';
 import DecryptionForm from '../components/decryptionForm';
 import EncryptionForm from '../components/encryptionForm';
-import Head from 'next/head';
 
 export default function Home() {
   const downloader = useRef();
@@ -10,35 +9,29 @@ export default function Home() {
   const [password, setPassword] = useState('');
 
   return (
-    <>
-      <Head>
-        <title>EasySave3 Editor</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Flex alignItems='center' justifyContent='center' mt='24' mb='14'>
-        <a ref={downloader} id={'downloader'} style={{ display: 'none' }}></a>
-        <Box
-          direction='column'
-          background='gray.700'
-          rounded='6'
-          p='12'
-          position='relative'
-        >
-          <Heading mb='6'>EasySave3 Editor</Heading>
+    <Flex alignItems='center' justifyContent='center' mt='24' mb='14'>
+      <a ref={downloader} id={'downloader'} style={{ display: 'none' }}></a>
+      <Box
+        direction='column'
+        background='gray.700'
+        rounded='6'
+        p='12'
+        position='relative'
+      >
+        <Heading mb='6'>EasySave3 Editor</Heading>
 
-          <Text>Password</Text>
-          <Input value={password} placeholder='a1bc2d3fghi4...' onChange={e => setPassword(e.target.value)} isDisabled={isOpening} isRequired />
-          <Text mb={6}>Use <Code>t36gref9u84y7f43g</Code> for Phasmophobia</Text>
+        <Text>Password</Text>
+        <Input value={password} placeholder='a1bc2d3fghi4...' onChange={e => setPassword(e.target.value)} isDisabled={isOpening} isRequired />
+        <Text mb={6}>Use <Code>t36gref9u84y7f43g</Code> for Phasmophobia</Text>
 
-          <Divider mt='5' mb='3' />
-          <Heading size='md' mb='3'>Decryption</Heading>
-          <DecryptionForm downloader={downloader} isOpening={isOpening} setIsOpening={setIsOpening} password={password} />
+        <Divider mt='5' mb='3' />
+        <Heading size='md' mb='3'>Decryption</Heading>
+        <DecryptionForm downloader={downloader} isOpening={isOpening} setIsOpening={setIsOpening} password={password} />
 
-          <Divider mt='5' mb='3' />
-          <Heading size='md' mb='3'>Encryption</Heading>
-          <EncryptionForm downloader={downloader} isOpening={isOpening} setIsOpening={setIsOpening} password={password} />
-        </Box>
-      </Flex>
-    </>
+        <Divider mt='5' mb='3' />
+        <Heading size='md' mb='3'>Encryption</Heading>
+        <EncryptionForm downloader={downloader} isOpening={isOpening} setIsOpening={setIsOpening} password={password} />
+      </Box>
+    </Flex>
   );
 }
