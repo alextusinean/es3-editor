2bff5820a8b56efd7f6984e441fa768ccc2bde95
Alex Tușinean <alex@tusinean.ro>
2023-07-28 17:48:54 +0000

    password for two more games and info about gzip

--- pages/index.js
+++ pages/index.js
@@ -46,15 +46,27 @@ export default function Home() {
 
               <Text>Password</Text>
               <Input value={password} placeholder='a1bc2d3fghi4...' onChange={e => setPassword(e.target.value)} isDisabled={isOpening} isRequired />
-              <Text mb={6}>Use <Code>t36gref9u84y7f43g</Code> for Phasmophobia</Text>
+              <Text>Use <Code>t36gref9u84y7f43g</Code> for Phasmophobia</Text>
+              <Text>Use <Code>6tr cr$#@%T#GFTVn</Code> for Strike Force Heroes</Text>
+              <Text mb={6}>Use <Code>browar23</Code> for Brewpub Simulator</Text>
 
               <Divider mt='5' mb='3' />
               <Heading size='md' mb='3'>Decryption</Heading>
               <DecryptionForm downloader={downloader} isOpening={isOpening} setIsOpening={setIsOpening} password={password} />
+              <Text mt='5'>If you are getting no errors after pressing the button</Text>
+              <Text>and the decrypted file starts downloading, it means that the</Text>
+              <Text>decryption was successful. For some games, the decrypted</Text>
+              <Text>save file might look like it's still encrypted, but</Text>
+              <Text>it can actually just be GZip compression. So, if your</Text>
+              <Text>decrypted file still looks like it's encrypted, please</Text>
+              <Text>try decompressing it with GZip.</Text>
 
               <Divider mt='5' mb='3' />
               <Heading size='md' mb='3'>Encryption</Heading>
               <EncryptionForm downloader={downloader} isOpening={isOpening} setIsOpening={setIsOpening} password={password} />
+              <Text mt='5'>If you had to decompress the save file after decryption,</Text>
+              <Text>you will have to recompress it again before uploading it here</Text>
+              <Text>and encrypt it again.</Text>
             </Box>
           </Flex>
 
