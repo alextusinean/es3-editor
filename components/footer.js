88543d04ecde6f50a451a61280cb3755a4d218b0
Kermit <138309022+tofted@users.noreply.github.com>
2025-10-15 20:01:41 +0400

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -8,7 +8,7 @@ export default function Footer({ left }) {
     <Box position='fixed' bottom={offset} left={left ? offset : undefined} right={left ? undefined : offset} textAlign={left ? 'left' : 'right'}>
       <Link as={NextLink} href='/terms-of-service' color='blue.500' mr='6.5px'>Terms of Service</Link>
       <Link as={NextLink} href='/privacy-policy' color='blue.500'>Privacy Policy</Link>
-      <Text>Made with <span style={{ color: 'red' }}>♥</span> by <Link as={NextLink} href='https://alex.tusinean.ro' color='blue.500'>Alex Tușinean</Link></Text>
+      <Text>Forked from href='https://alex.tusinean.ro' color='blue.500'>Alex Tușinean's Github</Link></Text>
     </Box>
   );
 }
