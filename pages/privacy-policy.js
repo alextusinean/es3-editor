4a470f7e3bd226d45a1510baf08861410214ab8f
Kermit <138309022+tofted@users.noreply.github.com>
2025-10-15 19:57:12 +0400

    privacy policy name replacement

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -48,7 +48,7 @@ The EasySave3 Editor Web Application prioritizes data security and takes reasona
 This Privacy Policy is effective as of the date stated above and will remain in effect except with respect to any changes in its provisions in the future, which will be in effect immediately after being posted on this page. We reserve the right to update or change our Privacy Policy at any time, and you should check this Privacy Policy periodically. Your continued use of the EasySave3 Editor Web Application after we post any modifications to the Privacy Policy on this page will constitute your acknowledgment of the modifications and your consent to abide and be bound by the modified Privacy Policy.<Dbr />
 
 5. Contact Us<br />
-If you have any questions or concerns about this Privacy Policy or the EasySave3 Editor Web Application, please contact Alex Tușinean at <Link as={NextLink} href='mailto:alex@tusinean.ro' color='blue.500'>alex@tusinean.ro</Link>.
+If you have any questions or concerns about this Privacy Policy or the EasySave3 Editor Web Application, please contact Alex Tușinean at <Link as={NextLink} href='mailto:calanmarshall@outlook.com' color='blue.500'>calanmarshall@outlook.com</Link>.
           </Text>
         </Box>
       </Flex>
